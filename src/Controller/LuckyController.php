0c9518019f633ba30b9fb42e29483ccceb29d308
Andrii Prykhodko <andriichello@gmail.com>
2025-07-14 07:32:28 +0300

    Create LuckyController

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('lucky/number', name: 'lucky_number', methods: ['GET'])]
+    #[Template('lucky/number.html.twig')]
+    public function number(): array
+    {
+        return ['number' => rand(0, 100)];
+    }
+}
