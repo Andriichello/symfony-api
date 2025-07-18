5ddd6214eeec017efecbca9972c770e8d2c186e0
Andrii Prykhodko <andriichello@gmail.com>
2025-07-18 19:51:54 +0300

    Install browser-kit, set up phpunit.xml

--- tests/Controller/AuthorControllerTest.php
+++ tests/Controller/AuthorControllerTest.php
@@ -4,8 +4,20 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+
+/**
+ * Class AuthorControllerTest.
+ *
+ * @package App\Tests\Controller
+ * @author Andrii Prykhodko <andriichello@gmail.com>
+ */
 final class AuthorControllerTest extends WebTestCase
 {
+    protected static function getKernelClass(): string
+    {
+        return 'App\Kernel';
+    }
+
     public function testIndex(): void
     {
         $client = static::createClient();
